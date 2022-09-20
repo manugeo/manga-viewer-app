14b928638a59b12192e3de19c2b5275f2c3b336e
Manu <manugeo13@gmail.com>
2022-09-20 17:01:53 +0530

    Added book-row and chapter-row with dummy data.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      background: 'pink',
+      height: '300px'
+    },
+    bookRow: {
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    selected: {
+      color: '#ffffff',
+      background: '#015a5a'
+    },
+    chapterRow: {
+      background: 'cyan',
+
+      margin: '4px',
+      display: 'flex',
+      justifyContent: 'center'
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div style={styles.bookRow}>
+        <button style={styles.selected}>balloon_dream</button>
+        <button>bound_eye_siora</button>
+        <button>raset</button>
+      </div>
+
+      <div style={styles.chapterRow}>
+        <button style={styles.selected}>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
     </div>
   );
 }
